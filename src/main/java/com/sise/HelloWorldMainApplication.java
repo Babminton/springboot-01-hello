e4364604edc76d79683fff724a7959cfbff721e7
Babminton <zengxiangqu@163.com>
2019-07-30 23:23:28 +0800

    helloworld-test

--- src/main/java/com/sise/HelloWorldMainApplication.java
+++ src/main/java/com/sise/HelloWorldMainApplication.java
@@ -21,6 +21,7 @@ public class HelloWorldMainApplication {
     public static void main(String[] args) {
         // Spring应用启动起来
         SpringApplication.run(HelloWorldMainApplication.class,args);
+        System.out.println("test");
     }
 
 
